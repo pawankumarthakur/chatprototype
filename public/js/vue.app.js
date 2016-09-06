dbf7435dff35bb2d97a65cb0184cc80f2592d7c6
Pawan Kumar <pawankumar@Pawans-Mac-mini.local>
2016-09-06 18:36:40 +0530

    Created Auth Migration

--- public/js/vue.app.js
+++ public/js/vue.app.js
@@ -1,1 +1,15 @@
-
+var vm = new Vue({
+    el: "#app-layout",
+    data: {
+        a: 1
+    },
+    created: function () {
+        // `this` points to the vm instance
+        console.log('a is: ' + this.a)
+    },
+    methods: {
+        pageload: function(){
+            console.log("Hello World");
+        }
+    }
+});
